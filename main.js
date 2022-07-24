b4dcb0cb56cd0722ba80d711f48361f2284f6b22
Fort45gaming <95743975+Fort45gaming@users.noreply.github.com>
2022-07-24 18:20:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/[...]',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+         random_number_g = Math.floor(Math.random() * 255) + 1; 
+         random_number_b = Math.floor(Math.random() * 255) + 1;
+         document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+          document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_label").innerHTML='I can hear-'+results[0].label;
+
+        document.getElementById("result_confidence").innerHTML='Accuracy-'+(results[0].confidence*100).toFixed(2)+"%";
+        img=document.getElementById('Jungle rustle');
+        img1=document.getElementById('bark');
+        img2=document.getElementById('meow');
+        img3=document.getElementById('roar');
+        img4=document.getElementById("moo")
+        if(results[0].label=="bark"){
+            img.src='bark.png';
+            img.src='bark.png';    
+        }
+        else if(results[0].label=="moo"){
+            img.src='cow.png';
+            img1.src='cow.png';
+             
+        }
+
+        else if(results[0].label=="roar"){
+            img.src='lion.png';
+            img1.src='lion.png'; 
+        }
+
+        else if(results[0].label=="meow"){
+            img.src='cat.png';
+            img.src='cat.png';
+        }
+
+        else{
+            img.src='tree.png';
+            img1.src='tree.png';
+        }
+    }
+    }
